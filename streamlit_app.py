971661aae3d59b2a6b3cd33480b02d5a335c2810
RoskoMx <78753834+RoskoMx@users.noreply.github.com>
2024-01-30 08:39:16 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -107,7 +107,7 @@ st.write("---")
 st.subheader(":tv: Nous pouvons faire une vidéo où nous parlons différentes langues")
 st.video(VIDEO_TUTORIAL, format="video/mp4", start_time=0)#Se puede modificar la hora de inicio
 #youtube_image = Image.open(DIR_ADJUNTOS / "Te regalo una.png")
-st.image(youtube_image)
+#st.image(youtube_image)
 
 
 # --- SECCIÓN DE PREGUNTAS Y RESPUESTAS ---
